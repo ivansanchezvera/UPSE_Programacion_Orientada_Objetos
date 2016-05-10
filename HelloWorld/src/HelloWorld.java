1c2f7e16bccaf7cb360757b8a4da9081640b0be6
Ivan <ivansanchez_1988@hotmail.com>
2016-05-10 14:27:17 -0500

    Hello World: Agregado primer programa en java terricolas

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Terricolas! Mueran!");
+		
+		int calculo = 0;
+		calculo = 1+ 55246;
+		
+		System.out.println("Segun mis calculos moriras en: " + calculo + " segundos.");
+	}
+
+}
